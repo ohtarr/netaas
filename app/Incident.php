2748c4b7e314e86b27b46367b21b541b507458ef
Ohtarr <ohtarr@gmail.com>
2020-06-17 15:44:18 -0500

    Configured incident to not create a service-now ticket if location is set to innactive or no monitoring

--- app/Incident.php
+++ app/Incident.php
@@ -344,6 +344,13 @@ class Incident extends Model
 /**/
 	public function createTicket()
 	{
+		$location = $this->get_location();
+		if($location->u_active == "true" || $location->u_priority == 0)
+		{
+			print "Location is deactivated or set to NO MONITORING, purging from system\n";
+			$this->purge();
+			return null;
+		}
 
 		print "Creating Ticket of type " . $this->IncidentType->name . "\n";
 		$ticket = ServiceNowIncident::create([
